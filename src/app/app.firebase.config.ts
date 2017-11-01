d999a21e48d74666a63e38511bcbe8f43b6d9c61
Андрей Анисимов <guitar8632@gmail.com>
2017-11-01 15:56:07 +0200

    Login and registation
    Login and registation

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA44UC2ULrv17w5stlZMfo9DOKYlyvlIF8",
+    authDomain: "searme-8dec5.firebaseapp.com",
+    databaseURL: "https://searme-8dec5.firebaseio.com",
+    projectId: "searme-8dec5",
+    storageBucket: "searme-8dec5.appspot.com",
+    messagingSenderId: "406945037747"
+  };
---
+++ src/pages/login/login.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { User } from "../../models/user"
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+
+  user = [] as User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  login(){
+
+  }
+
+  register(){
+    
+  }
+
+}
